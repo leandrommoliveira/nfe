9b2298a7cf6dedc290d2ec640a168c120ea5d492
JohannLucas <johann.lucas@hotmail.com>
2016-06-29 09:46:54 -0300

    Travis 2

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -5,11 +5,11 @@ class BuilderTest extends TestCase
     public function testGeracaoXML()
     {
         $xml_gerado = require __DIR__ . '/utils/builderXML.php';
-        $xml_gerado = preg_replace('/\s+/', "", $xml_gerado);
+        $xml_gerado = preg_replace('/\s+/', '', $xml_gerado);
         $xml_modelo = file_get_contents(__DIR__ . '/utils/xmlTeste.xml');
         $xml_modelo = str_replace("\r", '', $xml_modelo);
         $xml_modelo = str_replace("\n", '', $xml_modelo);
-        $xml_modelo = preg_replace('/\s+/', "", $xml_modelo);
+        $xml_modelo = preg_replace('/\s+/', '', $xml_modelo);
 
         $this->assertEquals($xml_modelo, $xml_gerado);
     }
