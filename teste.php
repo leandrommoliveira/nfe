893644242dc0c003c4067a2c14b2e5475522202d
JohannLucas <johann.lucas@hotmail.com>
2016-08-10 10:46:58 -0300

    Ajuste teste.php - Arquivo para teste

--- teste.php
+++ teste.php
@@ -1,22 +1,16 @@
 <?php
 
 include 'vendor/autoload.php';
-include 'src/Tools/Modulo11.php';
-
-//$cDV = \PhpNFe\Tools\::nfeCalculaDV('4216061822779500013055001000000045162102726');
 
 $path = __DIR__ . '/tests/utils/xmlTeste.xml';
 
 $path = __DIR__ . '/nota.xml';
 
-$mod = \PhpNFe\NFe\Tools\Modulo11::nfeCalculaDV('4216071822779500013055001000000201157616823');
-
 $cert = new \PhpNFe\Tools\Certificado\Certificado();
 $cert->carregarArquivo($path);
-$cert->carregarPfx(__DIR__ . '/../certificado_floripa.pfx', 'BOING');
+//$cert->carregarPfx(__DIR__ . 'seu_certificado.pfx', 'suaSenha');
 
 $xml = file_get_contents($path);
-
 
 $xml = $cert->assinarXML($xml, 'infNFe');
 
@@ -24,7 +18,6 @@ $nfe = new \PhpNFe\NFe\NFe($cert);
 
 $b = $cert->ehValido();
 
-//$v = \PhpNFe\Tools\Validar::validar($xml, __DIR__ . '/src/schemes/PL_008i2/enviNFe_v3.10.xsd');
 $v = $nfe->validar($xml, '3.10');
 
 $ret = $nfe->autorizar($xml);
