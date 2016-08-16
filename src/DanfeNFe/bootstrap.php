e0eca4a1631c409c5418e28848037d2090e5127a
Bruno Goncalves <bugotech@gmail.com>
2016-08-16 16:32:46 -0300

    resolver os testes

--- src/DanfeNFe/bootstrap.php
+++ src/DanfeNFe/bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 
-global $_ENV;
-throw new Exception(print_r($_ENV, true));
-
 // Registrar a DOMPDF_DIR
-$testing = (trim(@$_ENV['APP_ENV']) == 'testing');
-if (($testing != true) && (defined('DOMPDF_DIR') != true)) {
+if (defined('DOMPDF_DIR') != true) {
     define('DOMPDF_DIR', __DIR__ . '/../../../../dompdf/dompdf');
 }
